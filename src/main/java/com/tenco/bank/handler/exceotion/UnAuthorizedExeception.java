92270b2849ee78c24b9c8ad3d6aa39278525b967
Minhyeok <alsgur6811@gmail.com>
2024-08-05 15:51:39 +0900

    Advice 기본 설정 기능 추가

---
+++ src/main/java/com/tenco/bank/handler/exceotion/UnAuthorizedExeception.java
@@ -0,0 +1,18 @@
+package com.tenco.bank.handler.exceotion;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class UnAuthorizedExeception extends RuntimeException {
+
+	private HttpStatus status;
+	
+	// throw UnAuthorizedExecption
+	public UnAuthorizedExeception(String message, HttpStatus status) {
+		super(message);
+		this.status = status;
+	}
+	
+}
